f9ace6a9b76c8893fbb34cd5597e8349645cbd83
Kolter <wksaurey@gmail.com>
2024-10-31 12:22:37 -0600

    day2.2 complete

--- day2.py
+++ day2.py
@@ -4,17 +4,19 @@ commands = util.read_stripped_lines('input/day2.txt')
 
 xpos = 0
 ypos = 0
+aim = 0
 for command in commands:
     command = command.split()
     direction = command[0]
     distance = int(command[1])
 
     if direction == 'up':
-        ypos -= distance
+        aim -= distance
     elif direction == 'down':
-        ypos += distance
+        aim += distance
     else:
         xpos += distance
+        ypos += aim * distance
 
 print(f'x: {xpos}')
 print(f'y: {ypos}')
