bf326a20beecdca91ba5260d20d3429beb0d5435
Kolter <wksaurey@gmail.com>
2024-11-05 14:21:19 -0700

    day6.2 complete with circular buffer count

--- day6.py
+++ day6.py
@@ -1,19 +1,31 @@
 import util
-TOTAL_DAYS=80
+import time
+TOTAL_DAYS=256
 
 fish_scan_data = util.read_stripped_lines('input/day6.txt')[0].split(',')
 fish_data = list(map(int, fish_scan_data))
-# 0 is new fish, 1 has already repopulated
-# print(f'Initial state: {",".join(map(str, fish_data))}')
+print(f'Initial state: {",".join(map(str, fish_data))}')
 
+buffer = [0] * 7
+new_fish = [0] * 2
+for fish in fish_data:
+    buffer[fish] += 1
+pointer_index = 0
+print(buffer)
+print(new_fish)
+
+start = time.time()
 for day_index in range(TOTAL_DAYS):
-    new_fish = []
-    for fish_index, fish in enumerate(fish_data):
-        if fish == 0:
-            new_fish.append(8)
-            fish_data[fish_index] = 6
-        else:
-            fish_data[fish_index] -= 1
-    fish_data = fish_data + new_fish
-    # print(f'After {day_index} days: {",".join(map(str, fish_data))}')
-print(f'Fish Count: {len(fish_data)}')
+    # print(f'Day {day_index}')
+    temp = new_fish[0]    
+    new_fish[0] = new_fish[1]
+    new_fish[1] = buffer[pointer_index]
+    buffer[pointer_index] += temp
+    pointer_index += 1
+    if pointer_index == 7:
+        pointer_index = 0
+
+    print(f'Day: {day_index}')
+    print(f'Population {sum(buffer) + sum(new_fish)}')
+end = time.time()
+print(f'Run in {end-start} seconds')
