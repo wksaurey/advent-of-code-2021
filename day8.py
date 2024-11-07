00768a5b8edcaa85ca91394e827dd7d8bd13ef60
Kolter <wksaurey@gmail.com>
2024-11-07 15:32:53 -0700

    trying to find bug in day8.2

--- day8.py
+++ day8.py
@@ -1,16 +1,18 @@
 import util
 import sys
 
-file_path = 'input/day8_small_test.txt'
+file_path = 'input/day8_test.txt'
 value_signal_lengths = [6, 2, 5, 5, 4, 5, 6, 3, 7, 6]
 
 def main():
     decode_input = list(map(lambda input : [input[0].split(), input[1].split()], map(lambda input : input.split('|'), util.read_stripped_lines(file_path))))
+    result_total = 0
     for input in decode_input:
         number_lengths = [[] for _ in range(8)]
         solutions = [''] * 10
         signal_patterns = input[0]
         output_values = input[1]
+        # signal_patterns = ['abcefg', 'cf', 'acdeg', 'acdfg', 'bcdf', 'abdfg', 'abdefg', 'acf', 'abcdefg', 'abcdfg']
         
         for pattern in signal_patterns:
             number_lengths[len(pattern)].append(pattern)
@@ -42,11 +44,30 @@ def main():
                                 solutions[5] = number_lengths[5][index_0]
 
 
-                            
+        for pattern in number_lengths[6]:
+            if len(difference(solutions[1], pattern)) == 1:
+                solutions[6] = pattern
+            elif len(difference(solutions[3], pattern)) == 1:
+                solutions[0] = pattern
+        number_lengths[6].remove(solutions[0])
+        number_lengths[6].remove(solutions[6])
+        
 
-        print(solutions)
-        sys.exit(0)
-    print(f'Solution: {solutions}')
+        solutions[9] = number_lengths[6][0]
+
+        # print('Solution:')
+        # for index, solution in enumerate(solutions):
+        #     print(f'{index}: {solution}')
+
+        result = ''
+        for value in output_values:
+            for index, solution in enumerate(solutions):
+                if difference(value, solution) == '' and difference(solution, value) == '':
+                    result += str(index)
+                    break
+        print(result)
+        result_total += int(result)
+    print(f'Result Total: {result_total}')
 
 def difference(pattern_0, pattern_1):
     not_included = ''
